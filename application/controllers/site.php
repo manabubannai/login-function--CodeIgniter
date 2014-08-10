783931a55da2c0cb576875311ab81ce034572376
manabu <mnbmnb0524@gmail.com>
2014-08-10 14:20:12 +0800

    edit

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,14 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Site extends CI_Controller {
-
 	function __construct(){
 	            parent::__construct();
 		$this->is_logged_in();
 	}
+
 	function members_area(){
-		$this->load->view("members_area");
+		$data["main_content"] = "members_area";
+		$this->load->view("includes/template" ,$data);
 	}
+
 	function is_logged_in(){
 		$is_logged_in = $this->session->userdata("is_logged_in");
 
